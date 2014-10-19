a640655af49525f45347383d37dce4f5e19ac3f0
Mathias Fuchs <fuchsmat@students.unibe.ch>
2014-10-19 14:46:36 +0200

    y u no push?

---
+++ Skeleton/src/main/java/org/sample/model/UserRole.java
@@ -0,0 +1,49 @@
+package org.sample.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_roles")
+public class UserRole {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@ManyToOne
+	private User user;
+	
+	@Column(name = "role", nullable = false, length = 45)
+	private String role;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
