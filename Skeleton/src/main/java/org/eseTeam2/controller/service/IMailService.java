e739fc53c774106b7fdaab240fecfa78fd5abb3b
Mathias Fuchs <fuchsmat@students.unibe.ch>
2014-11-03 16:25:29 +0100

    added mail service, untested/unused yet

---
+++ Skeleton/src/main/java/org/eseTeam2/controller/service/IMailService.java
@@ -0,0 +1,6 @@
+package org.eseTeam2.controller.service;
+
+public interface IMailService {
+	public void sendEmail(String recipientEmail, String message, String title);
+
+}
---
+++ Skeleton/src/main/java/org/eseTeam2/controller/service/MailService.java
@@ -0,0 +1,29 @@
+package org.eseTeam2.controller.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class MailService implements IMailService {
+	
+	
+	 @Autowired
+	 private JavaMailSender mailSender;
+
+	public void sendEmail(String recipientEmail, String message,String title) {
+		
+	 
+         
+         
+        // creates a simple e-mail object
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(recipientEmail);
+        email.setSubject(title);
+        email.setText(message);
+         
+        // sends the e-mail
+        mailSender.send(email);
+		
+	}
+
+}
