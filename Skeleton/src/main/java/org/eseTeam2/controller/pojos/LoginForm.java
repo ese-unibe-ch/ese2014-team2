3a3b7076b169adcdadab69f369bc891a61ba98f9
Carl Balmer <balmer@carl.com>
2014-11-10 15:41:04 +0100

    Some Changes to Login form

--- Skeleton/src/main/java/org/eseTeam2/controller/pojos/LoginForm.java
+++ Skeleton/src/main/java/org/eseTeam2/controller/pojos/LoginForm.java
@@ -2,6 +2,7 @@ package org.eseTeam2.controller.pojos;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 /**
@@ -16,6 +17,7 @@ public class LoginForm {
 
     private Long id;
     @NotNull
+    @Size(min=6, max=20)
     private String password;
 
     @NotNull
