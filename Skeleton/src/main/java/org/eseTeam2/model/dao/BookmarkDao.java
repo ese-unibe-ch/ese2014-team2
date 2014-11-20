b6f794ced2aa62faa6ae833c18598d654a68b74c
Mathias Fuchs <fuchsmat@students.unibe.ch>
2014-11-20 12:59:56 +0100

    aaah

--- Skeleton/src/main/java/org/eseTeam2/model/dao/BookmarkDao.java
+++
@@ -1,8 +0,0 @@
-package org.eseTeam2.model.dao;
-
-import org.eseTeam2.model.Bookmark;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookmarkDao extends CrudRepository<Bookmark, Long>{
-
-}
